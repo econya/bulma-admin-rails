200838f2cc1451001c20bcd4f5fc093dfcacd51a
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2018-02-08 16:56:29 +0100

    improve navhelper doc a bit

--- app/helpers/nav_helper.rb
+++ app/helpers/nav_helper.rb
@@ -2,7 +2,9 @@ module NavHelper
   BULMA_IS_ACTIVE = 'is-active'.freeze
 
   # Returns the css class for an active element if current
-  # controller and action is within given list of controller#action s
+  # controller and action is within given list of controller#action s.
+  # If the given list contains entries without a '#' it kind of activates
+  # all given actions.
   def nav_class_if_active *active_routes
     current_controller,current_action = params[:controller], params[:action]
     active_routes.each do |route|
